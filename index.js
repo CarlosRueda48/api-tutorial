4600ed1a75eb3da93556b2724ded9bbd2ef8c17c
CarlosRueda <carlosruedag48@gmail.com>
2019-03-19 03:55:13 -0600

    Finished tutorial

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res)=>{
-    res.send(`Node express server is running on port ${PORT}`)
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useMongoClient: true
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-});
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
